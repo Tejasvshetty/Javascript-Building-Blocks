25d1548b5d4011eaad7d6db3d045d8ddddb459cc
Tejas Shetty <tejasvshetty@outlook.com>
2023-11-12 13:05:16 -0500

    11/12/2023 1:05pm

--- classes.js
+++ classes.js
@@ -1,15 +1,21 @@
-function MakeCar(carMake, carModel, carColor, numOfDoors){
-    this.make = carMake
-    this.model = carModel
-    this.color = carColor
-    this.doors = numOfDoors
-    this.lock = function(){
-        alert(`Locked $(this.doors) doors!`)
+class MakeCar
+    {
+        constructor(carMake, carModel, carColor, numOfDoors){
+            this.make = carMake
+            this.model = carModel
+            this.color = carColor
+            this.doors = numOfDoors
+        }
+        
+        lock(){
+            alert(`Locked $(this.doors) doors!`)
+        }
     }
-}
 let hondaCivic = new MakeCar('Honda', 'Civic', 'Silver', 4)
 let teslaRoadster = new MakeCar('Tesla', 'Roadser', 'Red', 2)
 
 teslaRoadster.bluetooth = true
 console.log (teslaRoadster.bluetooth) //true
 console.log (hondaCivic.bluetooth) //undefined
+
+// MakeCar.prototype.bluetooth = True 'Sets the property of all objects of type MakeCar

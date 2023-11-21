646105eb90d6455295a76d2979d14e201a11b3e8
Tejas Shetty <tejasvshetty@outlook.com>
2023-11-21 17:13:52 -0500

    trial2 file management

--- simpleServer/demo.js
+++ simpleServer/demo.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 http.createServer((req, res) =>{
-    fs.readFIle('helloWorld.html', (err, data) => {
+    fs.readFile('helloWorld.html', (err, data) => {
     res.writeHead(200, {'Content-Type': 'text/html'})
     res.write(data)
     res.end()
